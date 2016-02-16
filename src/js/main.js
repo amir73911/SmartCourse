8ddb54354c1ea87b8f7f8e8bc84dc38bdf98c312
Admin <614447dom>
2016-02-16 11:01:38 +0400

    upload files

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//= ../../bower_components/jquery/dist/jquery.js
+// ../../bower_components/modernizr/modernizr.js
+//= ../../bower_components/foundation-sites/dist/foundation.min.js
+
+$(document).foundation();
+
+//= partials/app.js
