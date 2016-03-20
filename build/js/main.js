9abe1d593783b0556395ca40a5e0ecbad6ba4ccf
Admin <614447dom>
2016-03-21 00:09:40 +0400

    Убрал скроллинг у карты

--- build/js/main.js
+++ build/js/main.js
@@ -9229,6 +9229,18 @@ this._core.settings.autoplayHoverPause&&this._core.is("rotating")&&this.play()},
 
 $(document).ready(function() {
     $(document).foundation();
+
+    $('#map_canvas').addClass('scrolloff'); // set the pointer events to none on doc ready
+    $('#map_canvas_wrapper').on('click', function () {
+        $('#map_canvas').removeClass('scrolloff'); // set the pointer events true on click
+    });
+
+    // you want to disable pointer events when the mouse leave the canvas area;
+
+    $("#map_canvas").mouseleave(function () {
+        $('#map_canvas').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+    });
+
 });
 
 $(document).ready(function () {
--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,18 @@
 
 $(document).ready(function() {
     $(document).foundation();
+
+    $('#map_canvas').addClass('scrolloff'); // set the pointer events to none on doc ready
+    $('#map_canvas_wrapper').on('click', function () {
+        $('#map_canvas').removeClass('scrolloff'); // set the pointer events true on click
+    });
+
+    // you want to disable pointer events when the mouse leave the canvas area;
+
+    $("#map_canvas").mouseleave(function () {
+        $('#map_canvas').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+    });
+
 });
 
 //= partials/vtabs.js
